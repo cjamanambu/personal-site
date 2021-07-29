0ba316f8d40ffc7bf827890708ab1d34d45e5837
CJ Amanambu <oamanambu@yahoo.com>
2021-07-29 13:40:31 +0100

    adds more to resume

--- app/Views/resume.php
+++ app/Views/resume.php
@@ -43,7 +43,8 @@
 							<div class="info-list">
 								<ul>
 									<li><strong>Name:</strong> Onyedika C. Amanambu</li>
-									<li><strong>Job:</strong> Software Engineer</li>
+									<li><strong>Job:</strong> Software Developer</li>
+									<li><strong>Role:</strong> Full-time/Freelance</li>
 									<li><strong>Location: </strong> Abuja, Nigeria</li>
 									<li><strong>Phone: </strong> +234(0)909 600 0024</li>
 									<li><strong>Email:</strong> mail@cjamanambu.com</li>
@@ -56,66 +57,87 @@
 				</div>
 
 				<!-- Resume -->
-<!--				<div class="section resume">-->
-<!--					<div class="content">-->
-<!--						<div class="cols">-->
-<!--							<div class="col col-md">-->
-<!--								<div class="title">-->
-<!--									<div class="title_inner">Experience</div>-->
-<!--								</div>-->
-<!--								<div class="resume-items">-->
-<!--									<div class="resume-item active">-->
-<!--										<div class="date">May 2020 - Present</div>-->
-<!--										<div class="name">Software Developer - <em>Connexxion Group</em></div>-->
-<!--										<p>-->
-<!--											Morbi nulla arcu, pellentesque sed egestas in, tempor eget felis. Nullam tincidunt augue in leo feugiat,-->
-<!--											quis interdum nisi sollicitudin.-->
-<!--										</p>-->
-<!--									</div>-->
-<!--									<div class="resume-item">-->
-<!--										<div class="date">June 2019 - January 2020</div>-->
-<!--										<div class="name">Software Developer - <em>OnTheStep</em></div>-->
-<!--										<p>-->
-<!--											Duis ipsum ligula, feugiat nec auctor vitae, porta sit amet urna. Praesent ex lorem, porta nec aliquet eu,-->
-<!--											ullamcorper eget nibh.-->
-<!--										</p>-->
-<!--									</div>-->
-<!--									<div class="resume-item">-->
-<!--										<div class="date">May 2018 - April 2019</div>-->
-<!--										<div class="name">Teaching Assistant / Peer Tutor - <em>University of Manitoba</em></div>-->
-<!--										<p>-->
-<!--											Morbi nulla arcu, pellentesque sed egestas in, tempor eget felis. Nullam tincidunt augue in leo feugiat,-->
-<!--											quis interdum nisi sollicitudin.-->
-<!--										</p>-->
-<!--									</div>-->
-<!--								</div>-->
-<!--							</div>-->
-<!--							<div class="col col-md">-->
-<!--								<div class="title">-->
-<!--									<div class="title_inner">Education</div>-->
-<!--								</div>-->
-<!--								<div class="resume-items">-->
-<!--									<div class="resume-item">-->
-<!--										<div class="date">2007 - 2008</div>-->
-<!--										<div class="name">Art University of New York</div>-->
-<!--										<p>-->
-<!--											Morbi nulla arcu, pellentesque sed egestas in, tempor eget felis. Nullam tincidunt augue in leo feugiat, quis-->
-<!--											interdum nisi sollicitudin.-->
-<!--										</p>-->
-<!--									</div>-->
-<!--									<div class="resume-item">-->
-<!--										<div class="date">2004 - 2006</div>-->
-<!--										<div class="name">Front-end Course</div>-->
-<!--										<p>-->
-<!--											Duis ipsum ligula, feugiat nec auctor vitae, porta sit amet urna. Praesent ex lorem, porta nec aliquet eu,-->
-<!--											ullamcorper eget nibh.-->
-<!--										</p>-->
-<!--									</div>-->
-<!--								</div>-->
-<!--							</div>-->
-<!--						</div>-->
-<!--					</div>-->
-<!--				</div>-->
+				<div class="section resume">
+					<div class="content">
+						<div class="cols">
+							<div class="col col-md">
+								<div class="title">
+									<div class="title_inner">Experience</div>
+								</div>
+								<div class="resume-items">
+									<div class="resume-item active">
+										<div class="date">May 2020 - PRESENT</div>
+										<div class="name">Software Developer - <em>Connexxion Group</em></div>
+                    <p style="margin-bottom: 10px;">
+                      - Collaborated in developing SaaS & bespoke business intelligence and financial technology systems
+                      using CodeIgniter & Laravel
+                    </p>
+                    <p style="margin-bottom: 10px;">
+                      - Streamlined collaboration and automated deployment of projects using Git which improved project completion and
+                      delivery by up to 40%.
+                    </p>
+                    <p>
+                      - Conducting product trainings, briefs and support for new and existing clients.
+                    </p>
+									</div>
+									<div class="resume-item">
+										<div class="date">June 2019 - January 2020</div>
+										<div class="name">Software Developer - <em>OnTheStep</em></div>
+										<p style="margin-bottom: 10px;">
+                      - Collaborated with design team to build robust user experiences using Typescript & Angular.
+                    </p>
+										<p style="margin-bottom: 10px;">
+                      - Worked with the business unit to automate transactions using Stripe API and improve the reliability
+                      of processing refunds by up to 15%.
+                    </p>
+                    <p>
+                      - Contributed to refactoring existing monolithic server code to a microservice architecture
+                      in Typescript, Express & NodeJS which improved code modularity by over 30%.
+                    </p>
+									</div>
+									<div class="resume-item">
+										<div class="date">May 2018 - April 2019</div>
+										<div class="name">Teaching Assistant / Peer Tutor - <em>University of Manitoba</em></div>
+										<p style="margin-bottom: 10px;">
+                      - Assessed student coding assignments done in a variety of programming languages and provided personalized
+                      feedback to each student.
+                    </p>
+                    <p style="margin-bottom: 10px;">
+                      - Provided educational support and guidance in introductory computer science concepts to 1st and 2nd year computer
+                      science students.
+                    </p>
+                    <p>
+                      - Mentored students on effective study strategies to prepare for hand-written coding exams and to complete
+                      coding projects.
+                    </p>
+									</div>
+								</div>
+							</div>
+							<div class="col col-md">
+								<div class="title">
+									<div class="title_inner">Education</div>
+								</div>
+								<div class="resume-items">
+									<div class="resume-item">
+										<div class="date">2014 - 2019</div>
+										<div class="name">Bachelor of Science - <em>University of Manitoba</em></div>
+										<p>- Major: Computer Science</p>
+										<p>- Minor: Business Management</p>
+                    <p>- Relevant Coursework: </p>
+                    <p style="margin-left: 25px">Software Engineering</p>
+                    <p style="margin-left: 25px">Web-Based Systems</p>
+                    <p style="margin-left: 25px">Grid/Cloud Computing</p>
+                    <p style="margin-left: 25px">Human-Computer Interaction</p>
+                    <p>- Co-Curricular:</p>
+                    <p style="margin-left: 25px">Instructor for the Science, Engineering and Technology (SET) Day in 2017.</p>
+                    <p style="margin-left: 25px">Contributor for the Autonomous Agents Lab robotics team for the FIRA 2018 competition.</p>
+
+                  </div>
+								</div>
+							</div>
+						</div>
+					</div>
+				</div>
 			</div>
 			<!-- Footer -->
 			<?php include('_footer.php');?>
